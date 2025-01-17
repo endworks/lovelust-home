7e2c511c55ce825ae7193b4da98c9eea060e8e86
ender <ender@end.works>
2025-01-17 15:49:53 +0100

    fix routing

--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,10 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path={import.meta.env.BASE_URL} element={<App />} />
-        <Route
-          path={import.meta.env.BASE_URL + "/privacy"}
-          element={<PrivacyPolicy />}
-        />
+        <Route path={import.meta.env.BASE_URL}>
+          <Route index element={<App />} />
+          <Route path="privacy" element={<PrivacyPolicy />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
