4c663b033eac444fe978d09eceb7a1300a5cb60b
ender <ender@end.works>
2025-01-17 20:11:08 +0100

    fix dark background

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ function App() {
         <main>
           <div className="relative isolate pt-14">
             <svg
-              className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+              className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 dark:stroke-gray-800 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
               aria-hidden="true"
             >
               <defs>
@@ -82,7 +82,11 @@ function App() {
                   <path d="M100 200V.5M.5 .5H200" fill="none" />
                 </pattern>
               </defs>
-              <svg x="50%" y="-1" className="overflow-visible fill-gray-50">
+              <svg
+                x="50%"
+                y="-1"
+                className="overflow-visible fill-gray-50 dark:fill-gray-950"
+              >
                 <path
                   d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
                   strokeWidth="0"
